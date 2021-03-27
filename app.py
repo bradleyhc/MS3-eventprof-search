8262ac363c16207d048cd2e187f7baca98bfb64e
bradleyhc <bradleyh.cooney@gmail.com>
2021-03-27 16:57:43 +0000

    (feat): connect register form to DB

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import (Flask, flash, redirect,
                    render_template, request, url_for, session)
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
-from bson.object import ObjectId
+from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
@@ -16,9 +16,42 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/home")
 def homepage():
+    return render_template("home.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+
+        # check for existing user email
+        user_exists = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()}
+        )
+
+        if user_exists:
+            flash("User with that email already exists, login instead")
+            return redirect(url_for("register"))
+
+        # check if user_type input switched to 'Project Owner'
+        u_type_input = request.form.get("user_type")
+        user_type = "freelancer" if u_type_input is None else "employer"
+
+        register = {
+            "first_name": request.form.get("first_name"),
+            "last_name": request.form.get("last_name"),
+            "email": request.form.get("email"),
+            "password": generate_password_hash(request.form.get("password")),
+            "user_type": user_type
+        }
+
+        mongo.db.users.insert_one(register)
+
+        flash("Reg successful")
+
     return render_template("home.html")
 
 
