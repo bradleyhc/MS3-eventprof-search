69a560e5b7fe3b5f80ee79aed93ea4bdce1e7be8
bradleyhc <bradleyh.cooney@gmail.com>
2021-03-27 12:59:40 +0000

    (feat): add Materialize Navbar to base.html

--- app.py
+++ app.py
@@ -3,9 +3,20 @@ from flask import (Flask, flash, redirect,
                    render_template, request, url_for, session)
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
-from bson.object import ObjectId
-from flask_pymongo import PyMongo
+#from bson.object import ObjectId
+#from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
 
-    
+app = Flask(__name__)    
+
+
+@app.route("/")
+@app.route("/home")
+def homepage():
+    return render_template("home.html")
+
+
+@app.route("/freelancers")
+def get_freelancers():
+    return render_template("freelancers.html")
