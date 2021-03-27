60828cf979e9c108ba520f6076e2b1e0eb53f211
bradleyhc <bradleyh.cooney@gmail.com>
2021-03-27 14:35:04 +0000

    (ops): connect App to MongoDB with PyMongo and dnspython

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from flask import (Flask, flash, redirect,
                    render_template, request, url_for, session)
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
-#from bson.object import ObjectId
-#from flask_pymongo import PyMongo
+from bson.object import ObjectId
+from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
 
-app = Flask(__name__)    
+app = Flask(__name__)
 
 app.secret_key = os.environ.get("SECRET_KEY")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
 
 @app.route("/")
 @app.route("/home")
